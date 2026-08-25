1b8d71ea581ac8a8290883a50b520a921bed36aa
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-08-25 14:48:43 -0300

    CriptoPasswd
    Sistema de criptografia de senha

--- api/src/main/java/com/classhub/api/Controller/LoginController.java
+++ api/src/main/java/com/classhub/api/Controller/LoginController.java
@@ -2,14 +2,13 @@ package com.classhub.api.Controller;
 
 import com.classhub.api.Models.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
