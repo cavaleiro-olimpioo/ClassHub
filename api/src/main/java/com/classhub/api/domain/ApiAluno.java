dbfd718083acbfcf915d95d4cbf5c359ab30673e
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-09-23 21:43:18 -0300

    CreateGeneratedValueAluno

--- api/src/main/java/com/classhub/api/domain/ApiAluno.java
+++ api/src/main/java/com/classhub/api/domain/ApiAluno.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDate;
+import java.time.Year;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Entity
 @Table(name = "api_alunos")
@@ -16,4 +18,13 @@ public class ApiAluno {
     @Column(nullable = false, unique = true) private String matricula;
     @Column(nullable = false) private LocalDate dataNascimento;
     @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "turma_id") private ApiTurma turma;
+
+    @PrePersist
+    private void gerarMatricula() {
+        if (matricula == null || matricula.isBlank()) {
+            int ano = Year.now().getValue();
+            int aleatorio = ThreadLocalRandom.current().nextInt(100000, 999999);
+            matricula = ano + String.valueOf(aleatorio);
+        }
+    }
 }
